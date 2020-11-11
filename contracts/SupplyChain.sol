f863de408f4b2d0b4d540855bdcf6ccc0ec5d850
chuqui <fgarlag@gmail.com>
2020-11-11 12:35:43 -0600

    State variables, enum and struct definitions

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,10 +12,13 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping (uint => Item) public items;
+
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -23,7 +26,8 @@ contract SupplyChain {
     Shipped
     Received
     (declaring them in this order is important for testing)
-  */
+   */
+  enum State {ForSale, Sold, Shipped, Received}
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
@@ -31,6 +35,16 @@ contract SupplyChain {
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
+  struct Item {
+    string name;
+    uint sku;
+    State state;
+    uint seller;
+    uint buyer;
+  }
+   
+
+
 
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
